1530bcad59d52118dadff006c76c18dabc08c359
NagarajMMN <105267768+NagarajMMN@users.noreply.github.com>
2022-05-10 21:05:23 +0530

    Add files via upload

---
+++ twowheeler.java
@@ -0,0 +1,23 @@
+package week1day1;
+
+public class twowheeler {
+	int noOfWheels= 2;
+	short noOfMirrors=2;
+	long chassisNumber=1896668796677l;
+	boolean isPunchured=false;
+	String bikeName= "RE";
+	double runningKM=1459;
+	
+	public static void main(String[] args) {
+     twowheeler object= new twowheeler();
+     System.out.println("noOfWheels   "+object.noOfWheels);
+     System.out.println("noOfMirrors   "+object.noOfMirrors);
+     System.out.println("chassisNumber  "+object.chassisNumber);
+     System.out.println("isPunchured  "+object.isPunchured);
+     System.out.println("bikeName    "+object.bikeName);
+     System.out.println("runningKM   "+object.runningKM);
+     
+     
+	}
+	
+}
